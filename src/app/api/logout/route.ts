105162a71d62220319c3d978860f9deb640480c7
Fernando Muniz <fernando.hon@live.com>
2025-07-13 18:33:04 -0300

    1.1.8

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,15 +1,15 @@
+// src/app/api/logout/route.ts
 import { NextResponse } from "next/server";
 
 export async function POST() {
   const baseUrl = process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
-  const loginUrl = new URL("/login", baseUrl);
 
-  const response = NextResponse.redirect(loginUrl);
+  const response = NextResponse.redirect(`${baseUrl}/login`);
 
   response.cookies.set("token", "", {
     path: "/",
     httpOnly: true,
-    maxAge: 0,
+    expires: new Date(0), // expira o cookie
   });
 
   return response;
