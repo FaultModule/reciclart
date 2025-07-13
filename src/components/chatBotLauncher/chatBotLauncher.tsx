244a2f2b29d9203093ac6e4c115f06a63555c752
Fernando Muniz <fernando.hon@live.com>
2025-07-13 17:36:06 -0300

    1.1.4

--- src/components/chatBotLauncher/chatBotLauncher.tsx
+++ src/components/chatBotLauncher/chatBotLauncher.tsx
@@ -1,10 +1,16 @@
 "use client";
+import Image from "next/image";
 import "./chatBotLauncher.css";
 
 export default function ChatBotLauncher() {
   return (
     <div className="chatbot-launcher">
-      <img className="" src="/logo-whatsapp.svg" alt="WhatsApp icon" />
+      <Image
+        src="/logo-whatsapp.svg"
+        alt="WhatsApp icon"
+        width={48} // ajuste conforme o tamanho desejado
+        height={48}
+      />
     </div>
   );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET || "default_secret";
